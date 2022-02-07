5dbe8caf114575f310d2b918682b08f4e2dfe671
ravgusha <ravgusha@gmail.com>
2022-02-07 14:43:08 +0300

    feat: add basic structure

--- src/index.js
+++ src/index.js
@@ -2,3 +2,24 @@ import './styles/main.scss';
 import '../node_modules/materialize-css/dist/css/materialize.min.css';
 import '../node_modules/materialize-css/dist/js/materialize.min';
 // import axios from 'axios';
+
+const tabs = document.getElementById('tabs');
+const contents = document.querySelectorAll('.content');
+
+function chooseChapter(e, page) {
+  // const color = e.target.style.backgroundColor;
+  const group = e.target.getAttribute('data-group');
+  contents.forEach((content) => {
+    content.classList.add('hidden');
+  });
+  document.querySelector('.active').classList.remove('active');
+  // document.querySelector('.hidden').classList.remove('hidden');
+  contents[group].classList.remove('hidden');
+
+  e.target.classList.add('active');
+}
+
+tabs.addEventListener('click', (e) => {
+  // console.log(e, group)
+  chooseChapter(e);
+});
