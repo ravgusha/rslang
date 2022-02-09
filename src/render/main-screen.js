7328f060bf179761f04570f645644a17243407c9
SergeyKagal <3461995@gmail.com>
2022-02-09 23:45:43 +0300

    fix: main screen styles

--- src/render/main-screen.js
+++ src/render/main-screen.js
@@ -85,11 +85,11 @@ const containerHtmlData = `
   </main>
 
   <footer class="footer">
-    <a href="#"><img class="rs-logo" src="./assets/images/svg/Asset 16300.svg" alt="rs logo" /></a>
+    <a href="#"><img class="rs-logo" src="./assets/images/svg/school-logo.svg" alt="rs logo" /></a>
     <div class="gh-links">
-      <a href="#"><img src="./assets/images/svg/Asset 15300.svg" alt="gh logo" />sergeykagal</a>
-      <a href="#"><img src="./assets/images/svg/Asset 15300.svg" alt="gh logo" />ravgusha</a>
-      <a href="#"><img src="./assets/images/svg/Asset 15300.svg" alt="gh logo" />angietune</a>
+      <a href="#"><img src="./assets/images/svg/gh-logo.svg" alt="gh logo" />sergeykagal</a>
+      <a href="#"><img src="./assets/images/svg/gh-logo.svg" alt="gh logo" />ravgusha</a>
+      <a href="#"><img src="./assets/images/svg/gh-logo.svg" alt="gh logo" />angietune</a>
     </div>
   </footer>
 </div>`;
