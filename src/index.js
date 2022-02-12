9f2052a3ffca5bd6710dfd646e0f323ca3767ffd
angietune <angietune@gmail.com>
2022-02-12 15:27:11 +0300

    feat: add routes and error page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 // import axios from 'axios';
 // import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.min.css';
-import screenListener from './app/listener';
-import renderMainScreen from './render/main-screen';
+// import containerHtmlData from './render/main-screen';
+import errorPage from './Pages/errorPage';
 import './styles/styles.scss';
+import routes from './routes';
+import screenListener from './app/listener';
+// eslint-disable-next-line no-restricted-globals
+const parseLocation = () => location.hash.slice(1).toLowerCase() || '/';
+
+const findComponentByPath = (path) => routes.find((r) => r.path.match(new RegExp(`^${path}$`))) || undefined;
+
+const router = () => {
+  const path = parseLocation();
+  const { component = errorPage } = findComponentByPath(path) || {};
+  document.body.innerHTML = component.render;
+};
+
+window.addEventListener('hashchange', router);
+window.addEventListener('load', router);
 
 const Game = {
   mode: 'main', // available modes for example : main, main-menu, e-book, sprint, audiocall, stat
   authorized: false, // if false -> render 6 chapters in e-book / else 7 chapters
 };
 export default Game;
-renderMainScreen();
-screenListener();
+// renderMainScreen();
+window.onload = () => {
+  screenListener();
+};
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import renderEbookScreen from './render/e-book';
+import renderMainScreen from './render/main-screen';
+import errorPage from './Pages/errorPage';
+
+export default [
+  { path: '/', component: renderMainScreen },
+  { path: '/ebook', component: renderEbookScreen },
+  { path: '/error', component: errorPage },
+  // { path: '/statistics', component: renderStatistics },
+  // { path: '/audiocall', component: renderAudiocall },
+  // { path: '/sprint', component: renderSprint },
+];
