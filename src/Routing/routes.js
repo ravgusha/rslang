47d5a643ca6fc3c1b2c3b9bc8c79c66f22a3c644
angietune <angietune@gmail.com>
2022-02-17 17:34:38 +0300

    feat: implement random words

--- src/Routing/routes.js
+++ src/Routing/routes.js
@@ -1,12 +1,8 @@
 import renderEbookScreen from '../render/e-book';
 import renderMainScreen from '../render/main-screen';
 import errorPage from '../Pages/errorPage';
-<<<<<<< HEAD
 import renderAudiocall from '../Pages/audiocallPage';
-=======
-import renderAudiocall from '../Pages/audiocall';
 import renderSprint from '../render/sprint-scr';
->>>>>>> d5bed99cce0c84a75b6f3f8a3bb84dc79225ce84
 
 export default [
   { path: '/', component: renderMainScreen },
