5cb0c305017311e2dad17974b038cfc5b3330646
SergeyKagal <3461995@gmail.com>
2022-02-21 19:10:37 +0300

    refactor: add correct links in footer

--- src/app/sprint/sprint-request.js
+++ src/app/sprint/sprint-request.js
@@ -5,6 +5,10 @@ const { default: axios } = require('axios');
 async function requestUnregWords() {
   const res = [];
   const data = await axios.get(`${BASE_URL}/words?page=0&group=0`);
+
+  if (data.status !== 200) {
+    console.log('Error');
+  }
   data.data.forEach((element) => {
     res.push(element.word);
   });
