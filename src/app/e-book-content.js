6e8f5b892ac5ecc9df9897560a3d9db42c6ed355
ravgusha <ravgusha@gmail.com>
2022-02-22 22:36:41 +0300

    feat: add words stat object

--- src/app/e-book-content.js
+++ src/app/e-book-content.js
@@ -21,7 +21,7 @@ function eBookContent() {
 
   async function getDifficultWords() {
     const res = await axios(
-      `${BASE_URL}/users/${userId}/aggregatedWords?filter={"$and":[{"userWord.difficulty":"hard"}]}`,
+      `${BASE_URL}/users/${userId}/aggregatedWords?wordsPerPage=100&filter={"userWord.difficulty":"hard"}`,
       {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -31,7 +31,6 @@ function eBookContent() {
       },
     );
     const data = await res.data[0].paginatedResults;
-
     contents[6].innerHTML = '';
 
     data.forEach((card) => {
@@ -86,6 +85,7 @@ function eBookContent() {
         </div>`,
       );
     });
+    countWords();
   }
 
   // Count of learnt words on the page
@@ -375,12 +375,17 @@ function eBookContent() {
 
   pagination.on('beforeMove', (event) => {
     currentPage = event.page - 1;
-    if (currentGroup === '6') return;
 
     localStorage.setItem('page', currentPage);
     localStorage.setItem('group', currentGroup);
 
-    getWords(currentGroup, currentPage);
+    if (currentGroup === '6') {
+      document.querySelector('.ebook__pag').style.opacity = '0';
+      getDifficultWords();
+    } else {
+      getWords(currentGroup, currentPage);
+      document.querySelector('.ebook__pag').style.opacity = '1';
+    }
   });
 
   function chooseChapter(e) {
@@ -398,10 +403,12 @@ function eBookContent() {
     document.querySelector('.active').classList.remove('active');
     // Up choosen tab
     e.target.classList.add('active');
-    pagination.movePageTo(0);
     if (currentGroup === '6') {
       getDifficultWords();
+      document.querySelector('.ebook__pag').classList.add('hidden');
     }
+    pagination.movePageTo(0);
+    document.querySelector('.ebook__pag').classList.remove('hidden');
   }
 
   tabs.addEventListener('click', (e) => {
@@ -462,6 +469,38 @@ function eBookContent() {
       addToLearnt(e);
     }
   });
+
+  const wordsStat = { numberOfDifficult: 0, numberOfLearnt: 0 };
+
+  async function countWords() {
+    const resDiff = await axios(
+      `${BASE_URL}/users/${userId}/aggregatedWords?&filter={"userWord.difficulty":"hard"}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+    const numberOfDifficult = await resDiff.data[0].totalCount[0].count;
+    wordsStat.numberOfDifficult = numberOfDifficult;
+
+    const resLearnt = await axios(
+      `${BASE_URL}/users/${userId}/aggregatedWords?&filter={"userWord.optional.status":"isLearnt"}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+    const numberOfLearnt = await resLearnt.data[0].totalCount[0].count;
+    wordsStat.numberOfLearnt = numberOfLearnt;
+
+    localStorage.setItem('wordsStat', JSON.stringify(wordsStat));
+  }
 }
 
 export default eBookContent;
